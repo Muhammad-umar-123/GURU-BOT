c1575027d6acadfa94aa2f5bc31d70efa388d9e2
Muhammad-umar-123 <153076917+Muhammad-umar-123@users.noreply.github.com>
2023-12-10 15:56:39 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['923136020697', 'umar', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
